dbcc2348a9919176663b7f987a2450ce66be8836
akhilvarma1 <61343260+akhilvarma1@users.noreply.github.com>
2021-01-19 18:48:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ let curDate = new Date();
 ReactDOM.render(
   <>
      <Header><h4>Hello Sir/mam {greeting}</h4></Header>
-     <h1 style={{color:'#ff5608',fontFamily:'pacifico',marginTop:'5em'}} className="text-center">Devices</h1>
+     <h1 style={{color:'#ff5608',fontFamily:'pacifico',marginTop:'5em'}} className="text-center">Devices..</h1>
     <Cards imgsrc="https://images.unsplash.com/photo-1542751371-adc38448a05e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
     cname="Gaming" />
     <Cards imgsrc="https://images.unsplash.com/photo-1574717024239-25253f4ef40a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
